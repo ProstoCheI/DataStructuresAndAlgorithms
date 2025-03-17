361f5670be23102ce7694360a4ee4e794fb0f1d8
ProstoCheI <shishkinm17@gmail.com>
2025-03-17 18:42:27 +0500

    Лр1 Убраны лишние сортировки

--- src/Lr1/NewClient.java
+++ src/Lr1/NewClient.java
@@ -9,45 +9,15 @@ public class NewClient {
         long end;
 
         int size = 200000;
-        BubbleSort bubbleArray = new BubbleSort(size);
-        InsertionSort insertionArray = new InsertionSort(size);
-        SelectionSort selectionArray = new SelectionSort(size);
         ShellSort shellSortArray = new ShellSort(size);
         ShellSort_Knuth shellSortKnuthArray = new ShellSort_Knuth(size);
         ShellSort_Sedgewick shellSortSedgewickArray = new ShellSort_Sedgewick(size);
 
         for (int i = 0; i < size; i++) {
-            bubbleArray.insert(random.nextLong(200000));
-            insertionArray.insert(random.nextLong(200000));
-            selectionArray.insert(random.nextLong(200000));
             shellSortArray.insert(random.nextLong(200000));
             shellSortKnuthArray.insert(random.nextLong(200000));
             shellSortSedgewickArray.insert(random.nextLong(200000));
         }
-
-        System.out.println("Bubble");
-        //bubbleArray.display();
-        start = System.currentTimeMillis();
-        bubbleArray.bubbleSort();
-        end = System.currentTimeMillis();
-        //bubbleArray.display();
-        System.out.printf("Время выполнения: %d мс \n", end - start);
-
-        System.out.println("\nInsertion");
-        //insertionArray.display();
-        start = System.currentTimeMillis();
-        insertionArray.insertionSort();
-        end = System.currentTimeMillis();
-        //insertionArray.display();
-        System.out.printf("Время выполнения: %d мс \n", end - start);
-
-        System.out.println("\nSelection");
-        //selectionArray.display();
-        start = System.currentTimeMillis();
-        selectionArray.selectionSort();
-        end = System.currentTimeMillis();
-        //selectionArray.display();
-        System.out.printf("Время выполнения: %d мс \n", end - start);
 
         System.out.println("\nShell");
         //shellSortArray.display();
